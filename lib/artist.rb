7bee62945d9a9c3205ae82b55af2699870347cb1
John Stassen <john.stassen@gmail.com>
2020-08-28 22:06:12 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,50 @@
+class Artist
+
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    @found = false
+    @@all.each do |artist|
+      if artist.name == name
+        return artist
+        @found = true
+      else
+      end
+    end
+    if @found == false
+      name = Artist.new(name)
+      return name
+    else
+    end
+  end
+
+  def print_songs
+    Song.all.each do |song|
+      if song.artist == self
+        puts song.name
+      else
+      end
+    end
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song
+
+  attr_accessor :artist, :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.new_by_filename(filename)
+    filename_array = filename.split(/[-]/)
+    song = filename_array[1].strip
+    new_instance = Song.new(song)
+    new_instance_artist = filename_array[0].strip
+    Artist.all.each do |artist|
+      if artist == new_instance_artist
+        new_instance.artist = artist
+      else
+      end
+    end
+  end
+
+  def name
+    @name
+  end
+
+end

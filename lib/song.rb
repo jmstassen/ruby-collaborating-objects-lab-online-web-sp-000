b0ce2a793be13718c236552abc358c0e4c332af4
John Stassen <john.stassen@gmail.com>
2020-08-28 22:57:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,7 @@ class Song
   def self.new_by_filename(filename)
     artist_name = filename.split(" - ")[0]
     song_name = filename.split(" - ")[1]
-    song = Song.new(song_name)
+    new_instance = Song.new(song_name)
     artist = Artist.find_or_create_by_name(artist_name)
     artist.songs << song
     song
